91bb6b2c217acde5f325e043e77b5e911daaa348
onestar1102 <gksquf6357@naver.com>
2025-09-15 11:13:05 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/example/warning/WarningApplication.java
@@ -0,0 +1,13 @@
+package com.example.warning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/warning/WarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
